8b3f7e367173f17a4ac5bda2410110c185fcc40a
mshanemc <shane.mclaughlin@salesforce.com>
2019-03-20 10:04:55 -0500

    init script, enable communities

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,8 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:github:package:install -g mshanemc -r electron-theme
+sfdx shane:github:package:install -g mshanemc -r electron-base
+sfdx force:user:permset:assign -n electron
 sfdx force:source:push
+sfdx shane:org:reauth -r --json
+sfdx shane:theme:activate -n Electron
 sfdx force:org:open
