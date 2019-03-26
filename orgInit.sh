b36eb1c81d0b524e68e06cca2303af334a9cd07b
mshanemc <shane.mclaughlin@salesforce.com>
2019-03-26 09:51:55 -0500

    all together now

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,10 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
+sfdx shane:connectedapp:uniquify -a identity/main/default/connectedApps/ExternalIdentity.connectedApp-meta.xml -p solution2
+
+sfdx shane:analytics:dataset:upload -f data/ea/deliveries.csv -m data/ea/deliveries.json -n deliveries -a SharedApp
+sfdx shane:analytics:dataset:upload -f data/ea/testdrives.csv -m data/ea/testdrives.json -n testdrives -a SharedApp
+sfdx shane:analytics:dataset:upload -f data/ea/csat.csv -m data/ea/csat.json -n csat -a SharedApp
+
 sfdx shane:github:package:install -g mshanemc -r electron-theme
 sfdx shane:github:package:install -g mshanemc -r electron-base
 sfdx shane:github:package:install -g mshanemc -r community-boilerplate
@@ -17,11 +23,13 @@ sfdx force:apex:execute -f scripts/DealerPerms.cls
 
 sfdx force:data:tree:import -p data/main/masterPlan.json 
 sfdx force:data:tree:import -p data/360/360plan.json
+sfdx force:data:record:create -s Account -v "Name=ExternalIDCustomers"
 
 sfdx shane:org:reauth -r --json
 sfdx force:user:password:generate
 
+# heroku connect section
 sfdx shane:heroku:repo:deploy -g mshanemc -r electron-web-app -n `basename "${PWD}"` -t autodeployed-demos
 heroku access:add platformpmmdemos@gmail.com -a `basename "${PWD}"` --permissions deploy,operate,manage
 heroku config:set DEALER_URL=`sfdx shane:communities:url --prefix dealers`  -a `basename "${PWD}"`
-heroku config:set DEALER_URL=`sfdx shane:communities:url --prefix customers`  -a `basename "${PWD}"`
+heroku config:set LOGIN_URL=`sfdx shane:communities:url --prefix externalid`/s/login  -a `basename "${PWD}"`
