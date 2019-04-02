f0af873ae34462339ca19f92f9c974002aa3525e
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-02 14:03:47 -0500

    destroy proper app name

--- destroy.sh
+++ destroy.sh
@@ -1,2 +1,3 @@
 sfdx shane:org:delete
-heroku apps:destroy -a `basename "${PWD}"` -c `basename "${PWD}"`
+heroku apps:destroy -a `basename "${PWD/mshanemc-/}"` -c `basename "${PWD/mshanemc-/}"`
+heroku apps:destroy -a `basename "${PWD/mshanemc-/}"`-stg -c `basename "${PWD/mshanemc-/}"`-stg
