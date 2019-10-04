1c1130f8a806f478394c316039bb3df88566a178
mshanemc <shane.mclaughlin@salesforce.com>
2019-10-04 16:43:54 -0500

    should probably include the alien weapon, too

--- orgInit.sh
+++ orgInit.sh
@@ -31,13 +31,13 @@ sfdx force:data:tree:import -p data/360/360plan.json
 
 sfdx force:user:password:generate
 
-# # heroku connect section
-# sfdx shane:heroku:repo:deploy -g mshanemc -r electron-web-app -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
-# # heroku access:add platformpmmdemos@gmail.com -a `basename "${PWD/mshanemc-/}"` --permissions deploy,operate,manage
-# # heroku config:set DEALER_URL=`sfdx shane:communities:url --prefix dealers`  -a `basename "${PWD/mshanemc-/}"`
-# # heroku config:set LOGIN_URL=`sfdx shane:communities:url --prefix externalid`/s/login  -a `basename "${PWD/mshanemc-/}"`
-# heroku pipelines:create `basename "${PWD/mshanemc-/}"` -a `basename "${PWD/mshanemc-/}"` -s production -t autodeployed-demos
-# heroku pipelines:connect `basename "${PWD/mshanemc-/}"` -r mshanemc/electron-web-app
-# sfdx shane:heroku:repo:deploy -g mshanemc -r electron-web-app -n `basename "${PWD/mshanemc-/}"`-stg -t autodeployed-demos
-# heroku pipelines:add `basename "${PWD/mshanemc-/}"` -a `basename "${PWD/mshanemc-/}"`-stg -s staging
-# sfdx shane:heroku:connect -a `basename "${PWD/mshanemc-/}"` -f assets/herokuConnect/electron-web.json -e custom
+# heroku connect section
+sfdx shane:heroku:repo:deploy -g mshanemc -r electron-web-app -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
+# heroku access:add platformpmmdemos@gmail.com -a `basename "${PWD/mshanemc-/}"` --permissions deploy,operate,manage
+# heroku config:set DEALER_URL=`sfdx shane:communities:url --prefix dealers`  -a `basename "${PWD/mshanemc-/}"`
+# heroku config:set LOGIN_URL=`sfdx shane:communities:url --prefix externalid`/s/login  -a `basename "${PWD/mshanemc-/}"`
+heroku pipelines:create `basename "${PWD/mshanemc-/}"` -a `basename "${PWD/mshanemc-/}"` -s production -t autodeployed-demos
+heroku pipelines:connect `basename "${PWD/mshanemc-/}"` -r mshanemc/electron-web-app
+sfdx shane:heroku:repo:deploy -g mshanemc -r electron-web-app -n `basename "${PWD/mshanemc-/}"`-stg -t autodeployed-demos
+heroku pipelines:add `basename "${PWD/mshanemc-/}"` -a `basename "${PWD/mshanemc-/}"`-stg -s staging
+sfdx shane:heroku:connect -a `basename "${PWD/mshanemc-/}"` -f assets/herokuConnect/electron-web.json -e custom
